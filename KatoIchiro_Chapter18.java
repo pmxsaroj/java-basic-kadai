23a5c1ca29c2bf68ef19f72f0f8d6a51214d7795
pmxsaroj <166109396+pmxsaroj@users.noreply.github.com>
2024-05-07 14:11:16 +0900

    Add files via upload

---
+++ KatoIchiro_Chapter18.java
@@ -0,0 +1,11 @@
+package kadai_018;
+
+public class KatoIchiro_Chapter18 extends Kato_Chapter18 {
+
+    public void setGivenName(String givenName){
+//        System.out.println("一郎");
+    }
+    public void eachIntroduce() {
+        System.out.println("趣味は読書です");
+    }
+}
