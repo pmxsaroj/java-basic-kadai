11560f4e6520659128fb362ce91550e1f01d8b43
pmxsaroj <166109396+pmxsaroj@users.noreply.github.com>
2024-05-08 11:11:49 +0900

    Update KatoHanako_Chapter18.java

--- kadai_018/KatoHanako_Chapter18.java
+++ kadai_018/KatoHanako_Chapter18.java
@@ -2,9 +2,10 @@ package kadai_018;
 
 public class KatoHanako_Chapter18 extends Kato_Chapter18 {
 
-        public void setGivenName(String givenName){
-//        System.out.println("花子");
+    public void setGivenName(){
+        super.givenName = "花子";
     }
+
     public void eachIntroduce(){
         System.out.println("好きな食べ物はリンゴです");
     }
